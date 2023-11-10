9af0f7aff01b36be48844676e584e5edd7e0ec2a
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2023-11-10 07:38:05 -0500

    WIP tomshley/brands/global/tware/tech/products/paste/paste-jammer#1
    https://gitlab.com/tomshley/brands/global/tware/tech/products/paste/paste-jammer/-/issues/1

--- src/main/scala/tomshley/brands/global/tware/tech/product/paste/jammer/infrastructure/config/JammerConfigBlocks.scala
+++
@@ -1,7 +0,0 @@
-package tomshley.brands.global.tware.tech.product.paste.jammer.infrastructure.config
-
-protected[jammer] enum JammerConfigBlocks(blockName: String) {
-  case JAMMER extends JammerConfigBlocks("paste.jammer")
-
-  def toBlockName: String = blockName
-}
