0e299199725f9f91528e7bc2768ff464decaaf4a
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2023-11-10 07:41:47 -0500

    WIP tomshley/brands/global/tware/tech/products/paste/paste-jammer#1
    https://gitlab.com/tomshley/brands/global/tware/tech/products/paste/paste-jammer/-/issues/1

--- src/main/scala/com/tomshley/brands/global/tware/tech/product/paste/jammer/app/JammerHandler.scala
+++ src/main/scala/com/tomshley/brands/global/tware/tech/product/paste/jammer/app/JammerHandler.scala
@@ -1,17 +1,11 @@
 package com.tomshley.brands.global.tware.tech.product.paste.jammer.app
 
-import akka.actor.ActorSystem
-import akka.http.caching.LfuCache
-import akka.http.caching.scaladsl.{Cache, CachingSettings}
 import akka.http.scaladsl.model.*
 import akka.http.scaladsl.model.StatusCodes.InternalServerError
 import akka.http.scaladsl.server.Directives.*
 import akka.http.scaladsl.server.PathMatchers.LongNumber
 import akka.http.scaladsl.server.{Directives, Route}
-
 import com.tomshley.brands.global.tech.tware.products.hexagonal.lib.runmainasfuture.http.routing.AkkaRestHandler
-
-import com.tomshley.brands.global.tware.tech.product.paste.common.config.PasteCommonConfigKeys
 import com.tomshley.brands.global.tware.tech.product.paste.jammer.core.models.JammerRequest
 import com.tomshley.brands.global.tware.tech.product.paste.jammer.core.ports.incoming.{JammerRequestMatched, ParseJammerRequestMatch, SinkJammerDependency}
 import com.tomshley.brands.global.tware.tech.product.paste.jammer.core.ports.outgoing.JammerCachedOrLoaded
@@ -26,6 +20,7 @@ sealed trait JammerHandler extends AkkaRestHandler {
 
   // /paste/1433279379/jawbone.utils.debounce%2Bv1.0%2Cpaste.cookie%2Bv1.0%2Cpaste.guid%2Bv1.0%2Cpaste.io.formdata%2Bv1.0%2Cpaste.oop%2Bv1.0%2Cpaste.util%2Bv1.0%2Cup.wellness.externals%2Bv1.0%2Cpaste.dom%2Bv1.0%2Cpaste.event%2Bv3.0%2Cpaste.has%2Bv3.0%2Cpaste.io%2Bv1.0%2Cjawbone.ui.responsive%2Bv1.0%2Cup.wellness.modals%2Bv1.0%2Cup.common.confirm_url_link%2Bv1.0.js
 
+  override lazy val routes: Seq[Route] = Seq(jammerGet)
   private lazy val jammerGet: Route =
     get {
       path(
@@ -63,6 +58,4 @@ sealed trait JammerHandler extends AkkaRestHandler {
 
       }
     }
-
-  override lazy val routes: Seq[Route] = Seq(jammerGet)
 }
